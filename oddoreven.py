ba89f5e0c67411d2c9ac64f7036629a2ec6d622c
Dharan <dharanigiri3017@gmail.com>
2026-09-12 11:26:44 +0530

    ODD OR EVEN

---
+++ oddoreven.py
@@ -0,0 +1,8 @@
+# Take input from the user
+num = int(input("Enter a number: "))
+
+# Check if the remainder is 0
+if num % 2 == 0:
+    print(f"{num} is an Even number.")
+else:
+    print(f"{num} is an Odd number.")
